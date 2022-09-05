8c2c6151f3f6873c5d5cab12858dcdc26296e0b9
amolmane7471 <maneamol3233@gmail.com>
2022-09-05 22:50:54 +0530

    [Amol] : added user registration UC1 to enter valid first name

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -1,4 +1,7 @@
 package com.bridgelabz.regex;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /*
  * purpose : User registration program to ensure all validations
  * @uthor : Amol
@@ -6,8 +9,27 @@ package com.bridgelabz.regex;
  */
 public class UserRegistration {
 
-	public static void main(String[] args) {
-		System.out.println("*** welcome to user registration program ***");	
+	static Scanner sc = new Scanner(System.in);
+	/*
+	 * UC1 : as a user need to enter valid first name
+	 * first name start with capital letters and has minimum 3 characters
+	 */
+	public static void validFirstName() {
+	
+		System.out.println("enter your first name");
+		String firstName = sc.next();
+	    boolean result = Pattern.matches("[A-Z]{1}[a-z]{2,}",firstName);
+        if(result)
+			System.out.println(firstName+" is a Valid First Name");
+		else
+			System.err.println(firstName+" is an Invalid First Name");
+	}
+	
+public static void main(String[] args) {
+	System.out.println("*** welcome to user registration program ***");	
+		validFirstName();
 	}
 
 }
+
+
