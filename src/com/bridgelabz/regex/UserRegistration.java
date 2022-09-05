a3253c133a525d2e33843d36d49fde4771c26381
amolmane7471 <maneamol3233@gmail.com>
2022-09-05 22:16:26 +0530

    [Amol] : added user registration system program

---
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.regex;
+/*
+ * purpose : User registration program to ensure all validations
+ * @uthor : Amol
+ * since : 2022/09/05
+ */
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("*** welcome to user registration program ***");	
+	}
+
+}
